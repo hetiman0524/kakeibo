2253d4645075a72ad9c07e2e8ae93c5307dc6011
hetiman0524 <heti0524.jdi@gmail.com>
2019-12-10 12:57:53 +0900

    forenkey追加

--- app/models/content.rb
+++ app/models/content.rb
@@ -1,5 +1,5 @@
 class Content < ApplicationRecord
   belongs_to :month
   belongs_to :category
-  belongs_to :user
+  belongs_to :user, foreign_key: :User_id
 end
--- db/migrate/20191210014121_add_user_id_to_contents.rb
+++
@@ -1,5 +0,0 @@
-class AddUserIdToContents < ActiveRecord::Migration[5.2]
-  def change
-    add_column :contents, :User_id, :integer
-  end
-end
