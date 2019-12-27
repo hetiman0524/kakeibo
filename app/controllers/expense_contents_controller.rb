b2ef5293e663869c1bcf1642b82e0ecc1eec00d4
hetiman0524 <heti0524.jdi@gmail.com>
2019-12-27 23:41:15 +0900

    expense_idパラメーター定義

--- app/controllers/expense_contents_controller.rb
+++ app/controllers/expense_contents_controller.rb
@@ -1,11 +1,19 @@
 class ExpenseContentsController < ApplicationController
   def edit
-   
+    @month =  Month.find(params[:expense_id])
+    @expense_content = ExpenseContent.where("month_id = #{@month.id}")
   end
 
   def update
+    expense_content = ExpenseContent.find(params[:id])
+    expense_content.update(expense_params)
   end
 
   def destroy
+  end
+
+  private
+  def expense_params
+    params.require(:expense_content).permit(:expense_category_id, :month_id, :content, :money).merge(user_id: current_user.id)
   end
 end
