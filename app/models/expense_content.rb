237d9aa0f28bebc03b350ba1a522ea7264060f77
hetiman0524 <heti0524.jdi@gmail.com>
2020-01-09 14:41:49 +0900

    README作成
    README入力

--- app/models/expense_content.rb
+++ app/models/expense_content.rb
@@ -1,6 +1,5 @@
 class ExpenseContent < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
-  
   belongs_to_active_hash :expense_category
   belongs_to_active_hash :month
   belongs_to :user
