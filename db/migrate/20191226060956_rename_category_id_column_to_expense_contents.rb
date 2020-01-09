99ad5939aeac441d7b8b0f176353d947faa5e0c6
hetiman0524 <heti0524.jdi@gmail.com>
2020-01-09 16:44:59 +0900

    前回のcommitと同様

--- db/migrate/20191226060956_rename_category_id_column_to_expense_contents.rb
+++ db/migrate/20191226060956_rename_category_id_column_to_expense_contents.rb
@@ -1,5 +1,5 @@
 class RenameCategoryIdColumnToExpenseContents < ActiveRecord::Migration[5.2]
   def change
-    rename_column :expense_contents, :category_id, :expense_category_id
+    rename_column :Expense_contents, :category_id, :expense_category_id
   end
 end
