7dbfa7cb9d73395e6465c323aa5623e61a2f1a2b
hetiman0524 <heti0524.jdi@gmail.com>
2020-01-09 16:42:19 +0900

    前回のcommitと同様

--- db/migrate/20191225053828_rename_contents_to_expense_contents.rb
+++ db/migrate/20191225053828_rename_contents_to_expense_contents.rb
@@ -1,5 +1,5 @@
 class RenameContentsToExpenseContents < ActiveRecord::Migration[5.2]
   def change
-    rename_table :Contents, :Expense_contents
+    rename_table :contents, :Expense_contents
   end
 end
